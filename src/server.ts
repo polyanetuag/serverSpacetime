7a841ed3fe8de7d2417110171adcab8ab687a81b
Polyane Tuag <pollyportugues@hotmail.com>
2023-05-22 14:37:57 -0300

    feat: route for uploads

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,19 @@ import fastify from "fastify";
 import { memoriesRoutes } from "./routes/memories";
 import cors from "@fastify/cors";
 import jwt from "@fastify/jwt";
+import multipart from "@fastify/multipart";
 import { authRoutes } from "./routes/auth";
+import { uploadRoutes } from "./routes/upload";
+import { resolve } from "node:path";
 
 const app = fastify()
+
+app.register(multipart) 
+
+app.register(require('@fastify/static'), {
+    root: resolve(__dirname, '../uploads'),
+    prefix: '/uploads'
+})
 
 app.register(cors, {
     origin: true
@@ -17,6 +27,7 @@ app.register(jwt, {
 
 app.register(authRoutes)
 app.register(memoriesRoutes)
+app.register(uploadRoutes)
 
 app.listen({
     port: 3333,
